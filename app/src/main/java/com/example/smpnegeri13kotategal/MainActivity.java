28ef1e4b2b8032436957c2b5929af97b37a8cc0b
aenuns23 <aenunsafitri6@gmail.com>
2022-03-04 10:13:19 +0700

    smp n 13 tegal

--- app/src/main/java/com/example/smpnegeri13kotategal/MainActivity.java
+++ app/src/main/java/com/example/smpnegeri13kotategal/MainActivity.java
@@ -1,35 +1,32 @@
 package com.example.smpnegeri13kotategal;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
-import com.smarteist.autoimageslider.SliderAnimations;
-import com.smarteist.autoimageslider.SliderView;
+import com.example.smpnegeri13kotategal.EkskulActivity;
+import com.example.smpnegeri13kotategal.FasilitasActivity;
+import com.example.smpnegeri13kotategal.GaleriActivity;
+import com.example.smpnegeri13kotategal.GuruActivity;
+import com.example.smpnegeri13kotategal.PerpusActivity;
+import com.example.smpnegeri13kotategal.ProfilActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-        SliderView sliderView;
-        int[] images = {R.drawable.headersmp13,
-                R.drawable.header1,
-                R.drawable.profilsekolahsmp,
-                R.drawable.headersmp13,
+    int[] images = {R.drawable.smp13,
+            R.drawable.headersmp13,
+            R.drawable.profilsekolahsmp,
+            R.drawable.header1
         };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sliderView = findViewById(R.id.image_slider);
 
-        SliderAdapter sliderAdapter = new SliderAdapter(images);
-
-        sliderView.setSliderAdapter(sliderAdapter);
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
-        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
-        sliderView.startAutoCycle();
     }
 
     public void profil(View view) {
@@ -37,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void guru(View view) {
-        Intent intent = new Intent(MainActivity.this, GuruActivity.class);
+    public void galeri(View view) {
+        Intent intent = new Intent(MainActivity.this, GaleriActivity.class);
         startActivity(intent);
     }
 
@@ -47,18 +44,18 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void fasilitas(View view) {
-        Intent intent = new Intent(MainActivity.this, FasilitasActivity.class);
-        startActivity(intent);
-    }
-
     public void perpus(View view) {
         Intent intent = new Intent(MainActivity.this, PerpusActivity.class);
         startActivity(intent);
     }
 
-    public void galeri(View view) {
-        Intent intent = new Intent(MainActivity.this, GaleriActivity.class);
+    public void guru(View view) {
+        Intent intent = new Intent(MainActivity.this, GuruActivity.class);
+        startActivity(intent);
+    }
+
+    public void fasilitas(View view) {
+        Intent intent = new Intent(MainActivity.this, FasilitasActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/smpnegeri13kotategal/PrestasiActivity.java
+++ app/src/main/java/com/example/smpnegeri13kotategal/PrestasiActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GaleriActivity extends AppCompatActivity {
+public class PrestasiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
